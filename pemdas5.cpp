98c7b0c99c9c3315ef0b50f4786382cb0a257e86
studane <zhidaneprabowoguzlend@gmail.com>
2026-02-23 21:12:04 +0700

    fifth commit

---
+++ pemdas5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){ //mulai
+    int a, t, luas;
+
+    //langkah 2
+    cout << "Masukkan Alas : ";
+    cin >> a;
+
+    //langkah 3
+    cout << "Masukkan Tinggi : ";
+    cin >> t;
+
+    //langkah 4
+    luas = a * t;
+
+    //langkah 5
+    cout << "Hasilnya : " << luas;
+
+}//selesai
